42c7fa1d31300d5da95a1d2af7fba5d9ef0cc2e6
Arpita Verma <a8729060@gmail.com>
2026-06-24 12:05:20 +0530

    Add username field to AuthRequest class
    Modified AuthRequest class to include username field and update constructors and methods accordingly.

--- src/main/java/com/vcsm/security/dto/AuthRequest.java
+++ src/main/java/com/vcsm/security/dto/AuthRequest.java
@@ -1,19 +1,43 @@
 package com.vcsm.security.dto;
 
 public class AuthRequest {
+
+    private String username;
     private String email;
     private String password;
 
     public AuthRequest() {}
 
-    public AuthRequest(String email, String password) {
+    public AuthRequest(String username, String email, String password) {
+        this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getUsername() {
+        return username != null ? username : email;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        if (this.username == null) {
+            this.username = email;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
