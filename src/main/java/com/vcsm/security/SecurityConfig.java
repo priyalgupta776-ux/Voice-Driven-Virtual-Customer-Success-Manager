f27c0d10c25288a7031939f2bd56e23f2f7951d0
KRITIKA-l <kritika0925@gmail.com>
2026-06-26 09:51:24 +0530

    Inject UserDetailsService in security config

--- src/main/java/com/vcsm/security/SecurityConfig.java
+++ src/main/java/com/vcsm/security/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
 
     @Autowired
     private HmacAuthenticationFilter hmacAuthenticationFilter;
+
+    @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
     @Bean
